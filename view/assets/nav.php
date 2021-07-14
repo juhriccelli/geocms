42a6cb15b0eb3f50c27209e2a94cba89ad31cc21
Juliano <juliano.riccelli@gmail.com>
2021-07-14 17:52:44 -0300

    Commit Inicial

---
+++ view/assets/nav.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css"> <!-- Bootstrap -->
+  <link rel="stylesheet" href="assets/css/ol.css"> <!-- OpenLayers -->
+  <script src="assets/js/jquery-3.6.0.min.js"></script> <!-- Jquery -->
+  <script src="assets/js/bootstrap.min.js"></script> <!-- Bootstrap -->
+  <script src="assets/js/ol.js"></script> <!-- OpenLayers -->
+</head>
+
+<body>
+  
+</body>
+
+</html>
---
+++ view/index.php
@@ -0,0 +1,12 @@
+<?php
+//Inserir o CSS e o Javascript
+include_once('assets/nav.php');
+?>
+
+ <!DOCTYPE HTML>
+ <html lang="pt-br">
+ <body>
+
+
+ </body>
+ </html>
