82f92b74439f088776c7f76da8506e0b4d9294cc
Juliano <juliano.riccelli@gmail.com>
2021-08-19 17:31:47 -0300

    Versão 0.2.2

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+include("pages/config.php");
+include("pages/navbar.php");
+?>
+
+
+
+<body>
+  <div class="mapa" id="ajax">
+    <div class="indexConteudo">
+      <div class="indexInfo">
+
+      </div>
+      <div class="indexMundo">
+        <?php //echo file_get_contents("assets/img/mundo.svg"); ?>
+      </div>
+    </div>
+  </div>
+  <div class="rodape">
+    <div class="competicao" id="brasileiro">
+      <img src="assets/img/campeonatos/brasileiro.png" height="100%"/>
+    </div>
+
+    <div class="competicao" id="libertadores">
+      <img src="assets/img/campeonatos/libertadores.png" height="100%"/>
+    </div>
+
+    <div class="competicao" id="ucl">
+      <img src="assets/img/campeonatos/ucl.png" height="100%"/>
+    </div>
+
+    <div class="competicao" id="nba">
+      <img src="assets/img/campeonatos/nba.png" height="100%"/>
+    </div>
+
+    <div class="competicao" id="nfl">
+      <img src="assets/img/campeonatos/nfl.png" height="100%"/>
+    </div>
+
+    <div class="competicao" id="mlb">
+      <img src="assets/img/campeonatos/mlb.png" height="100%"/>
+    </div>
+
+
+  </div>
+
+
+</body>
---
+++ pages/mapa.php
@@ -0,0 +1,19 @@
+<?php
+//include("config.php")
+
+if (isset($_GET["competicao"])) {
+	$competicao = $_GET["competicao"];
+}
+
+/*$brk = [40.6826465, -73.9754156];
+$lks = [34.0430175, -118.2694428];*/
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+	<div id='map' style="width: 100%; height: 100%;">
+		<script src="assets/js/mapa.js"></script> <!-- JS MAPA  -->
+	</div>
+</body>
+</html>
